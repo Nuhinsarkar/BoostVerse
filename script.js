1fb8c0eec4557623e69405ca5af2a62031089862
Md Nuhin Sarker <mnuhinsarkar@gmail.com>
2026-07-12 04:25:01 +0600

    Initial BoostVerse website

---
+++ script.js
@@ -0,0 +1,14 @@
+AOS.init();
+
+var typed = new Typed("#typing", {
+    strings: [
+        "SEO Experts",
+        "Web Designers",
+        "Social Media Managers",
+        "Google Ads Experts",
+        "AI Solution Provider"
+    ],
+    typeSpeed: 60,
+    backSpeed: 40,
+    loop: true
+});
